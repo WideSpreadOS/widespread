d61fe576db999831c1f1641f9173ebbd82953846
WideSpreadOS <84348146+WideSpreadOS@users.noreply.github.com>
2022-02-15 07:03:02 -0500

    added all routes/views for News Zone with working Zone nav

--- routes/news.js
+++ routes/news.js
@@ -7,12 +7,46 @@ const { ensureAuthenticated } = require('../config/auth');
 const User = require('../models/User');
 
 
+/* NEWS */
+
 router.get('/', async (req, res) => {
-    res.render('news/home')
+    res.render('news/home', { subZone: "Home", zone: 'News', subZonePage: 'Home' })
+});
+
+router.get('/local', async (req, res) => {
+    res.render('news/local', { subZone: "Local", zone: 'News', subZonePage: 'Local' })
+});
+
+router.get('/national', async (req, res) => {
+    res.render('news/national', { subZone: "National", zone: 'News', subZonePage: 'National' })
+});
+
+router.get('/international', async (req, res) => {
+    res.render('news/international', { subZone: "International", zone: 'News', subZonePage: 'International' })
+});
+
+router.get('/business', async (req, res) => {
+    res.render('news/business', { subZone: "Business", zone: 'News', subZonePage: 'Business' })
+});
+
+router.get('/health', async (req, res) => {
+    res.render('news/health', { subZone: "Health", zone: 'News', subZonePage: 'Health' })
+});
+
+router.get('/technology', async (req, res) => {
+    res.render('news/technology', { subZone: "Technology", zone: 'News', subZonePage: 'Technology' })
+});
+
+router.get('/politics', async (req, res) => {
+    res.render('news/politics', { subZone: "Politics", zone: 'News', subZonePage: 'Politics' })
+});
+
+router.get('/events', async (req, res) => {
+    res.render('news/events', { subZone: "Events", zone: 'News', subZonePage: 'Events' })
 });
 
 
-/* NEWS */
+/* NEWS SEARCH */
 
 router.get('/results', (req, res) => {
     res.render('news/results')
