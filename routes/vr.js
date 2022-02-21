7d2d7558b31bd67332111f5ebdc73a8ceeb6cdfb
WideSpreadOS <84348146+WideSpreadOS@users.noreply.github.com>
2022-02-21 15:35:25 -0500

    added academy business news leisure wellness and help pages for VR

--- routes/vr.js
+++ routes/vr.js
@@ -10,6 +10,10 @@ const { readUserData, allUserPosts, getUserPosts } = require("../modules/user/us
 const User = require('../models/User');
 const Post = require('../models/Post');
 const UserPhoto = require('../models/UserPhoto');
+const Company = require('../models/Company');
+const Course = require('../models/Course');
+const Class = require('../models/Class');
+const LearningPoint = require('../models/LearningPoint');
 
 
 
@@ -26,6 +30,51 @@ router.get('/users/dashboard', ensureAuthenticated, async (req, res) => {
     res.render('vr/dashboard', { layout: 'vr', currentPageTitle: 'VR', currentUser, userPhotos, user, posts
  });
 })
+
+
+// Academy
+router.get('/academy', async (req, res) => {
+    const courses = await Course.find()
+    res.render('vr/academy/home', { layout: 'vr', currentPageTitle: 'VR Academy', courses})
+});
+
+
+// Business
+router.get('/business', async (req, res) => {
+    const companies = Company.find()
+    res.render('vr/business/home', { layout: 'vr', currentPageTitle: 'VR Business', companies })
+});
+
+// News
+router.get('/news', async (req, res) => {
+
+    res.render('vr/news/home', { layout: 'vr', currentPageTitle: 'VR Business' })
+});
+
+// Leisure
+router.get('/leisure', async (req, res) => {
+
+    res.render('vr/leisure/home', { layout: 'vr', currentPageTitle: 'VR Leisure' })
+});
+
+// Wellness
+router.get('/wellness', async (req, res) => {
+
+    res.render('vr/wellness/home', { layout: 'vr', currentPageTitle: 'VR Wellness' })
+});
+
+// Things
+router.get('/things', async (req, res) => {
+
+    res.render('vr/things/home', { layout: 'vr', currentPageTitle: 'VR Things' })
+});
+
+// Help
+router.get('/help', async (req, res) => {
+
+    res.render('vr/help/home', { layout: 'vr', currentPageTitle: 'VR Help' })
+});
+
 
 
 module.exports = router;
